1009698f2a05348a86078dd79153cff309d57bd2
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-07-17 17:46:56 +0530

    Variables

--- 03_JavaScript/Variables.js
+++ 03_JavaScript/Variables.js
@@ -7,3 +7,10 @@ const c = 56;
 console.log(a);
 console.log(b);
 console.log(c);
+
+// Variables Rule
+// 1. Variable name contain english alphabets, digits, underscore & dollar. No other character can be used
+// ex: let var_name = "XYZ";    Ex: "Syntax Error"
+// 2.  Variaable name cannot be start with number/ digit.
+// 3. Variable name are case-sensitive.
+// 4. You cannot use a keyword for a variable name.
