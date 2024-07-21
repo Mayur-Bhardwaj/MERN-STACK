e0de953dbb80c4781c49c35965aeb5d270ce6b05
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-07-21 18:00:05 +0530

    ArithmeticOperator

---
+++ 03_JavaScript/Operator/ArithmeticOperator.js
@@ -0,0 +1,9 @@
+let x = 2;
+let  y = 3;
+console.log(x+y); // 5
+console.log(x-y); // -1
+console.log(x*y); // 6
+console.log(x/y); // 0.6666666666666666
+console.log(x%y); // 2
+
+console.log(2**3); // 8    console.log(x**y);
