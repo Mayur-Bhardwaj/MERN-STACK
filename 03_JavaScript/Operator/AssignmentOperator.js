c146aa9f2e496606f8b9547d7a961e6611997458
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-07-21 18:19:09 +0530

    Assignment Operator

---
+++ 03_JavaScript/Operator/AssignmentOperator.js
@@ -0,0 +1,19 @@
+let x = 5;
+x = x+5;
+console.log(x); //10
+
+ x += 5;
+ console.log(x); // 15
+ 
+
+ x -= 5;
+ console.log(x); // 10
+
+ x *= 5;
+ console.log(x); // 50
+
+ x /= 5;
+ console.log(x); // 10
+
+ x %= 5;
+ console.log(x); // 0
