7154bffc175c7c68f8a89cd5e950a26d1833931b
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-07-21 17:31:47 +0530

    Data Types and Operators

---
+++ 03_JavaScript/Operator/DataTypes&Opeators.js
@@ -0,0 +1,17 @@
+// 1. Numberes
+// let minusZero = -0;
+// console.log(minusZero);     // -0
+
+// 2. Strings
+
+// 3. Boolean
+
+// 4. Null and Undefined
+
+// 5. BigInt --> A Normal number can store max(2^53-1) & minimum (-2^53-1).
+//After this beyond we use "Big Int".
+
+let verBigNumber = 634872687249827782787238737634847584n;
+console.log(verBigNumber); // Output = 634872687249827782787238737634847584n
+
+// 6.Objects --> To store the data in key value pair we use Objects
