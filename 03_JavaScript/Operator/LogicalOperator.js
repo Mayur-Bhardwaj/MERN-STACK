5247447d5d44f021562b3299029fb61decf877b3
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-07-21 18:49:00 +0530

    Logical Operator

---
+++ 03_JavaScript/Operator/LogicalOperator.js
@@ -0,0 +1,18 @@
+//AND Operator
+
+console.log(false && false);    //Output:- false
+console.log(false && true);     //Output:-  false
+console.log(true && false);     //Output:- false
+console.log(true && true);      //Output:- true
+
+//OR Operator
+
+console.log(false || false);    //Output:- false
+console.log(false || true);     //Output:-  false
+console.log(true || false);     //Output:- false
+console.log(true || true);      //Output:- true
+
+//NOT Operator
+
+console.log(!false);    //Output:- true
+console.log(!true);     //Output:- false
