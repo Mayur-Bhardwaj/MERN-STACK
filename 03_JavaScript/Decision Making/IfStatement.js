ec575be03b05968812af75186c454a734cb535eb
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-07-22 20:03:46 +0530

    If Statement

---
+++ 03_JavaScript/Decision Making/IfStatement.js
@@ -0,0 +1,27 @@
+// console.log("Starting....");
+
+// let isUserSignedIn = true;
+
+// if ( isUserSignedIn ) {
+//     console.log("Show the Dashboard.");
+// }
+
+/*
+Output:- 
+
+Starting....
+Show the Dashboard.
+
+*/
+
+// ----------------------------------------------------------------------------------------------
+
+console.log("Starting....");
+
+let isUserSignedIn = false;
+
+if ( isUserSignedIn ) {
+    console.log("Show the Dashboard.");
+}
+
+//Output:-   Starting....
