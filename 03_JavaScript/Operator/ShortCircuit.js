8e655b966ba952d6e447cd85ec87e3efc1da53d5
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-07-22 16:46:11 +0530

    ShortCircuit

---
+++ 03_JavaScript/Operator/ShortCircuit.js
@@ -0,0 +1,9 @@
+console.log(10 && 30);      // output:- 30 
+
+//Because 10 and 30 both are truethy values . 
+//If in AND Operation the first operand is truethy then they return  2nd value.
+
+console.log(0 && 30);       //Output:- 0
+
+//Because 0 is falsy value in AND(&&) Opearation. So they return first Opearand Value.
+
