e3956314a5cd523a9dbaf17894a403425d7b08b8
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-07-22 18:44:51 +0530

    Comparision Operator

---
+++ 03_JavaScript/Operator/ComparisionOperator.js
@@ -0,0 +1,18 @@
+console.log(10 > 20);   //Output:- False
+
+console.log(10 < 20);  //Output:- True
+
+console.log(5<=5);      //Output:- True
+
+
+// == --> Abstract Equality Operator
+
+console.log(10 == 10);  //Output:- True
+
+console.log("10" == 10);  //Output:- True
+
+// == --> Strict Equality Operator
+
+console.log(10 === 10);     //Output:- True
+
+console.log("10" === 10);   //Output:- False
