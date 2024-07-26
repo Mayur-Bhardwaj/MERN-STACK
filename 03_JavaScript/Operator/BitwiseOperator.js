fefe0d19ddc7ff2cef8d7ecd71f8413f4d476031
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-07-26 15:11:07 +0530

    Bitwise Operator

--- 03_JavaScript/Operator/BitwiseOperator.js
+++ 03_JavaScript/Operator/BitwiseOperator.js
@@ -4,3 +4,13 @@ let b = 5;
 console.log(a | b); //Bitwise OR    // Output:-  5
 
 console.log(a & b); //Bitwise AND  // Output:-  4
+
+console.log(a ^ b);  // Bitwise XOR  // Output:- 1
+
+console.log(4 << 5); //Left Shift   // Output:- 128
+
+// or 
+
+console.log(1 << 3);  //Left Shift   // Output:- 8
+
+console.log(27 >> 2); //Right Shift   // Output:- 6
