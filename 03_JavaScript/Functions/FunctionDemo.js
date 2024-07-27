d38f914b9a6909d6e0061ee9962c859bb2c24ab6
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-07-27 18:02:54 +0530

    Function

---
+++ 03_JavaScript/Functions/FunctionDemo.js
@@ -0,0 +1,8 @@
+//Using the below syntax we can define our function.
+
+function greetingToEveryOne() {
+    console.log("My Logic Line 1");
+    console.log("My Logic Line 2");
+}
+
+greetingToEveryOne();
