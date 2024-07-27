95877f5f590f4b9f4c17d71c8b34196a7bf7f46e
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-07-27 17:07:20 +0530

    Break & Continue

--- 03_JavaScript/Loops/Break&Continue.js
+++ 03_JavaScript/Loops/Break&Continue.js
@@ -1,7 +1,20 @@
+//Break
+
+// let i = 1;
+// while(i<=10){
+//     if(i%7==0)
+//         break;              //Output:- 1,2,3,4,5,6.
+//     console.log(i);
+//     i+=1;
+// }
+
+// console.log("end");
+
 let i = 1;
 
 while(i <=10 ){
     if(i%7 == 0){
+        i++;    //Output:- 1,2,3,4,5,6,7,8,9,10
         continue;
     }
     console.log(i);
