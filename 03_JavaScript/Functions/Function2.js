dffd2ca558494bdb7aa0e2ecad76c9a314adf2ae
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-07-30 14:55:21 +0530

    1 Parameter is given

---
+++ 03_JavaScript/Functions/Function2.js
@@ -0,0 +1,10 @@
+function isEvenOrOdd(x){     // Function Definition
+    if( x%2 == 0 ){
+        console.log("Even Number");
+    }else{
+        console.log("Odd Number");
+    }
+}
+
+// isEvenOrOdd(10);    //Function Calling      //Output:- Even Number
+isEvenOrOdd(9);     //Output:- Odd Number
