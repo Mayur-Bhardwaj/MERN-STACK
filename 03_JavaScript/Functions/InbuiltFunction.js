3e6b7eded12716b00b7fa6fd944999c676417a05
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-08-02 14:34:16 +0530

    Inbuilt Maths Function

---
+++ 03_JavaScript/Functions/InbuiltFunction.js
@@ -0,0 +1,18 @@
+// console.log(Math.abs(8));        //Output:- 8
+// console.log(Math.abs(-8));       //Output:- 8
+// console.log(Math.floor(8.7));   //Output:- 8  They give smaller value
+// console.log(Math.floor(8.3));      //Output:- 8
+// console.log(Math.ceil(8.7));      //Output:- 9
+// console.log(Math.ceil(8.3));     //Output:- 9  They five larger value
+// console.log(Math.round(8.3));    //Output:- 8
+// console.log(Math.round(8.7));    //Output:- 9
+// console.log(Math.round(8.2));    //Output:- 8
+// console.log(Math.round(8.5));    //Output:- 9
+// console.log(Math.round(8.4));     //Output:- 8
+// console.log(Math.min(12,-8,-55,56));    //Output:- -55
+// console.log(Math.max(12,-8,-55,56));    //Output:- 56
+console.log(Math.min(-12,-8,-55,-56));    //Output:- -56
+console.log(Math.max(-12,-8,-55,-56));    //Output:- -8
+console.log(Math.sqrt(50));    //Output:- 7.0710678118654755
+console.log(Math.random());     //Output:- 0.09669288492306083      It will give the random no. between 0 & 1.
+
