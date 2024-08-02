30aa7f5ad331d46fe345d807bffccf5f6add0441
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-08-02 16:03:04 +0530

    Strings

---
+++ 03_JavaScript/Strings/StringDeclaration.js
@@ -0,0 +1,27 @@
+//Ways to make a strings\
+
+let s1 = "This is the First Ways of Creating Strings";
+console.log(s1);
+
+let s2 = 'This is the second way to create a string';
+console.log(s2);
+
+let s3 = `This is the third way to create a string`;
+console.log(s3);
+
+//Operations on strings
+
+let s4 = ' Rahul said "Where are you going ?" ';
+console.log(s4);
+
+let s5 = "I don't have code. ";
+console.log(s5);
+
+let s6 = `Wow What a day "wow" don't `;     //We use both the single and double quotes.
+console.log(s6); 
+
+//String Polation / Templated Strings.
+
+let x = 10;
+let s7 = `Value of x is ${x}`;
+console.log(s7);          //  Output:- Value of x is 10 Also called Templated Strings.
