fcbed3785baf7e9f0c04e53c6472de37b409a1b7
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-08-02 14:06:20 +0530

    Rest Parameter with Function

--- 03_JavaScript/Functions/RestWithFunction.js
+++ 03_JavaScript/Functions/RestWithFunction.js
@@ -3,3 +3,5 @@ function fun(...values){
 }
 
 fun(10,20,30,40,50);    //Output:- [ 10, 20, 30, 40, 50 ]
+fun(10, 20, 30, 40, 50, "Mayur", true );    //Output:-    [ 10, 20, 30, 40, 50, 'Mayur', true ]
+// Arrays are heterogenous means at one time they can store the number, string or boolean.
