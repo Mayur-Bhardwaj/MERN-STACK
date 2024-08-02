f656ba96eaeabcbaf0fa0f0ebf80b7334f0fc902
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-08-02 13:53:50 +0530

    Multiple Parameter senf

--- 03_JavaScript/Functions/DefaultParameter.js
+++ 03_JavaScript/Functions/DefaultParameter.js
@@ -17,12 +17,22 @@
 // }
 // console.log(add(10,20));    //Output:- 30
 
+// function add(x,y=5,z){
+//     console.log(x);          //Output:- 10 
+//     console.log(y);          //Output:- 5
+//     console.log(z);          //Output:- undefined
+
+//     return x+y+z;             //Output:- NaN
+// }
+
+// console.log(add(10));
+
 function add(x,y=5,z){
     console.log(x);          //Output:- 10 
-    console.log(y);          //Output:- 5
-    console.log(z);          //Output:- undefined
+    console.log(y);          //Output:- 55
+    console.log(z);          //Output:- 5
 
-    return x+y+z;             //Output:- NaN
+    return x+y+z;             //Output:- 70
 }
 
-console.log(add(10));
+console.log(add(10,55,5));
