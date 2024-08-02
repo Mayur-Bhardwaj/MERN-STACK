87e25d53be7b5e9eee0a5ff5ac3ea04cdd22b144
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-08-02 13:51:45 +0530

    Default Parameter

--- 03_JavaScript/Functions/DefaultParameter.js
+++ 03_JavaScript/Functions/DefaultParameter.js
@@ -12,7 +12,17 @@
 // console.log(add(10));   //Output:- 15
 // Default Parameter
 
-function add(x, y=5){
-    return x+y;
+// function add(x, y=5){
+//     return x+y;
+// }
+// console.log(add(10,20));    //Output:- 30
+
+function add(x,y=5,z){
+    console.log(x);          //Output:- 10 
+    console.log(y);          //Output:- 5
+    console.log(z);          //Output:- undefined
+
+    return x+y+z;             //Output:- NaN
 }
-console.log(add(10,20));    //Output:- 30
+
+console.log(add(10));
