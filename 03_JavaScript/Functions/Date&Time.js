3cddbc30ed0f1c236f2c3c1cf8626a93dc8aa1f7
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-08-02 15:32:25 +0530

    Date&Time

---
+++ 03_JavaScript/Functions/Date&Time.js
@@ -0,0 +1,9 @@
+const d = new Date();
+console.log(d);     //Output:- 2024-08-02T09:54:54.699Z
+console.log(d.getDate());   //Output:-  2 
+console.log(d.getFullYear());  //Output:- 2024
+console.log(d.getDay());  //Output:- 5
+console.log(d.getMonth());  //Output:- 7    //Month is start from 0 , Jan-0, Feb-1,....
+console.log(d.getHours());  //Output:- 15
+console.log(d.getMinutes());  //Output:- 26
+console.log(d.getSeconds());  //Output:- 7
