dde4f6c4d4f01693c6363fa83e7ee8aa822d0315
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-08-03 13:43:24 +0530

    Add files via upload

--- 03_JavaScript/Functions/EccapeSequence.js
+++ 03_JavaScript/Functions/EccapeSequence.js
@@ -1,7 +1,19 @@
-const s1 = "Hello \n World";
-const s2 = "New \t World";
+// const s1 = "Hello \n World";
+// const s2 = "New \t World";
 
-console.log(s1);    //Output:- Hello 
-                    //           World
-                    
-console.log(s2);    //Output:- New 	 World
+// console.log(s1);    //Output:- Hello 
+//                     //           World
+
+// console.log(s2);    //Output:- New 	 World
+
+// const str = "Ram said \" I am Good \" ";
+// console.log(str);       //Output:- Ram said " I am Good " 
+
+// const str2 = `Ram said "I am Good" give me a code of \`12345\``;
+// console.log(str2);      //Output:- Ram said "I am Good" give me a code of `12345`
+
+// String Concatination
+
+const s1 = "Hello World";
+const s2 = "New World";
+console.log(s1 + " " +s2);    //Output:- Hello World New World
