089247291fc9bd718b6f05a87fa978e89034caca
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-08-03 13:22:07 +0530

    Escape Sequence

---
+++ 03_JavaScript/Functions/EccapeSequence.js
@@ -0,0 +1,7 @@
+const s1 = "Hello \n World";
+const s2 = "New \t World";
+
+console.log(s1);    //Output:- Hello 
+                    //           World
+                    
+console.log(s2);    //Output:- New 	 World
