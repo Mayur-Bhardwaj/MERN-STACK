4eadaa62bc488911f9efa8b5910bf195fe7fd3f5
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-08-07 17:08:10 +0530

    Add files via upload

---
+++ 03_JavaScript/Conditional Problems/Demo1.js
@@ -0,0 +1,10 @@
+let isUSerPremium = false;
+let isUserVIP = true;
+
+if(isUSerPremium){
+    console.log("Enjoy the content of PREMIUM on HotStar");
+} else if(isUserVIP){
+    console.log("Enjoy the content of VIP on HotStar, for more content subscribe to PREMIUM");
+}else{
+    console.log("Plese purchase the either PREMIUM or VIP content to enjoy on HotStar");
+}
