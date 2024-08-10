51db846e24fbd4e31e977757f00d357e3b228f15
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-08-10 15:31:50 +0530

    Even Or Odd

---
+++ 03_JavaScript/Conditional Problems/EvenOrOdd.js
@@ -0,0 +1,19 @@
+//Check the given number is even or odd
+
+let x = 34;
+if (x % 2 == 0) {
+  console.log("Even Number");
+} else {
+  console.log("Odd Number");
+}
+
+//Output:- Even Number
+
+let y = 57;
+if(y%2 == 0){
+    console.log("Even Number");
+} else {
+    console.log("Odd Number");
+}
+
+//Output:- Odd Number
