1ea43220d181d5caf02536d6794ace145919ff46
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-08-10 16:27:01 +0530

    Smallest Among three no.

---
+++ 03_JavaScript/Conditional Problems/SmallestAmong3.js
@@ -0,0 +1,15 @@
+//Check which number is smaller among the 3 number
+
+//let x = 4, y = 8, z = 2;
+
+let x = 4;
+let y = 8;
+let z = 2;
+
+if(x<y && x<z){
+    console.log("The First Number is Smaller.");
+} else if(y<x && y<z){
+    console.log("The Second Number is Smaller.");
+} else{
+    console.log("The Third Number is Smaller.");
+}
