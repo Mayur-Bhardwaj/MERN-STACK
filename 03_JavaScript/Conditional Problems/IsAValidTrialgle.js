ff79bf0ee7e08dec071aa631ad6bc8b58bad7f2e
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-08-10 16:39:48 +0530

    Given triangle is valid or not

---
+++ 03_JavaScript/Conditional Problems/IsAValidTrialgle.js
@@ -0,0 +1,11 @@
+let a = 5; 
+let b = 6; 
+let c = 10;
+
+if(a+b>c && b+c>a && a+c>b){
+    console.log("Yes it is a valid triangle");
+} else {
+    console.log("No, it is not a valid triangle");
+}
+
+//Output:-  Yes it is a valid triangle
