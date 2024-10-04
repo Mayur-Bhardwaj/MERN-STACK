7b6c5012ea95ec420a6eca8052e70f34217f465a
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-10-04 13:40:07 +0530

    Update

--- 03_JavaScript/Scopes/Mutability.js
+++ 03_JavaScript/Scopes/Mutability.js
@@ -4,19 +4,19 @@
 // If we cannot do the modifications then this is called "Immutability".
 // If we want the data will be immutable then we initialize with "const" keyword.
 
-let x = 100;
-x = 200;
-console.log("The value of x is: ", x);  //Example of mutability //O/p:- 200
+// let x = 100;
+// x = 200;
+// console.log("The value of x is: ", x);  //Example of mutability //O/p:- 200
 
-var y = 100;
-y = 200;
-console.log("The value of y is: ", y);  //Example of mutability //O/p:- 200
+// var y = 100;
+// y = 200;
+// console.log("The value of y is: ", y);  //Example of mutability //O/p:- 200
 
 //Immutability
 
-const z = 500;
-z = 450;
-console.log("The value of z is: ", z);
+// const z = 500;
+// z = 450;
+// console.log("The value of z is: ", z);
 // It throws an error. 
 // TypeError: Assignment to constant variable.
 
@@ -28,7 +28,7 @@ console.log("The value of z is: ", z);
 
 // const a; // O/p:- 'const' must be initialized.
 
-const a = undefined;  //This will be run.
+// const a = undefined;  //This will be run.
 
 // Example of immutability is Let's say we have a DB connection, there is very less cases where we want to 
 // update the configuration of this DB connection. Most of the time connection object is setup and don't
@@ -42,3 +42,16 @@ const a = undefined;  //This will be run.
 // Cons 
 // We need to always in mind what to make mutable and what to make immutable.
 // HAndling it in object is tricky. 
+
+const obj = {
+    x:10,               //O/p: { x: 10, y: 20 }
+    y:20,
+};
+console.log(obj);
+
+obj.z = 100; //Addition of more key value pair is allowed
+console.log(obj);      //O/p:-  { x:10, y: 20, z: 100 }
+
+obj = {a:10}
+console.log(obj);       //O/p:- TypeError: Assignment to constant variable.
+
