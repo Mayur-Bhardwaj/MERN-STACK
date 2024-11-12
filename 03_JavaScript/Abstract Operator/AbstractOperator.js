f60a3b96a4567ad9c0bb4c2cd6ee64463b08faae
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-11-12 16:58:37 +0530

    Abstract Operator

--- 03_JavaScript/Abstract Operator/AbstractOperator.js
+++ 03_JavaScript/Abstract Operator/AbstractOperator.js
@@ -15,3 +15,17 @@ var z = "false";
 console.log(!z);      //false 
 
 // Note:- !(NOT Operator, boolean ke upper lagne ke liye bnaya gya hai) 3 is no. but js internally convert into boolean.This is called Implicit Type Conversion.
+
+const number  = 1 + '2';
+console.log(number);    //12
+
+const no = 1 - '2';
+console.log(no);    //-1   
+// In JS documentaation is written that by using the ToNumber() we called subtract. 1st left side operand convert then right side operand value convert into number.
+// If expression is written then they first solve the expression.  "-" operator is converted both operand into the number. 
+
+let a = 1 + null;   //both are primitive  (1+null) --> (1+0) = 1
+console.log(a);     //1
+
+let b = 1 + undefined;  //both primitive either of them are not string so convert  both into no.
+console.log(b);     //NaN
