fce417e55b1d78a2ebf8f8b2cd46ee9d0ed53899
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2024-11-12 15:42:12 +0530

    AbstractOperator

--- 03_JavaScript/Abstract Operator/AbstractOperator.js
+++ 03_JavaScript/Abstract Operator/AbstractOperator.js
@@ -1,1 +1,17 @@
+// Type Conversion Types:- 1. Explicit Type Conversion.   2. Implicit Type Conversion. 
+//1. Explicit Type Conversion:-  These conversion is done by the debelopers. Manually we convert the one type to another.
+// 2. 1. Implicit Type Conversion:-  These conversion is done by JS from one type to another type.
 
+let x = 3; 
+console.log(!x);     //false. Because 3 is truthy value and not make it falsy value.
+
+const obj={}
+console.log(!obj);      //false
+
+let y = "";
+console.log(!y);    //true Because the empty string gives the falsy value.
+
+var z = "false";
+console.log(!z);      //false 
+
+// Note:- !(NOT Operator, boolean ke upper lagne ke liye bnaya gya hai) 3 is no. but js internally convert into boolean.This is called Implicit Type Conversion.
