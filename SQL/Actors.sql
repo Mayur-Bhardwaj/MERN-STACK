c58d4ff51343b8619988c5158b24090a4994edd6
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-01-18 16:40:42 +0530

    Add the fields

--- SQL/Actors.sql
+++ SQL/Actors.sql
@@ -42,3 +42,53 @@ DESC ACTORS;
 | GENDER  | enum('MALE','FEMALE','TRANSGENDER') | YES  |     | NULL    |       |
 | CHARGES | decimal(10,0)                       | YES  |     | NULL    |       |
 +---------+-------------------------------------+------+-----+---------+-------+
+
+CREATE TABLE ACTOR(
+	NAME VARCHAR(20),
+    GENDER ENUM("MALE", "FEMALE", "TRANSGENDER"),
+    CHARGES DECIMAL,
+    ID INT AUTO_INCREMENT,
+    PRIMARY KEY(ID)
+    );
+
+ CREATE TABLE ACTOR(
+    -> Name VARCHAR(50) NOT NULL,
+    -> GENDER ENUM("MALE", "FEMALE", "TRANSGENDER") NOT NULL,
+    -> CHARGES DECIMAL,
+    -> ID INT AUTO_INCREMENT,
+    -> PRIMARY KEY(ID)
+    -> );
+
+-- Insert the data 
+-- For Single Entry
+INSERT INTO ACTORS VALUES ("AKSHAY KUMAR", "MALE", 300000);
+INSERT INTO ACTORS (NAME, GENDER, CHARGES) VALUES ("VARUN DHAWAN", "MALE", 500000);
+
+-- For Multiple Entry
+INSERT INTO ACTORS VALUES ("VICKEY KAUSHAL", "MALE", 500000), ("ALIYA BHATT", "FEMALE", 400000);
+
+-- Retrieve everything from table
+
+ SELECT * FROM ACTORS;
+
++----------------+--------+---------+
+| NAME           | GENDER | CHARGES |
++----------------+--------+---------+
+| AKSHAY KUMAR   | MALE   |  300000 |
+| VARUN DHAWAN   | MALE   |  500000 |
+| VICKEY KAUSHAL | MALE   |  500000 |
+| ALIYA BHATT    | FEMALE |  400000 |
++----------------+--------+---------+
+
+-- Select only name and gender from the table.
+
+SELECT NAME, GENDER FROM ACTORS;
+
++----------------+--------+
+| NAME           | GENDER |
++----------------+--------+
+| AKSHAY KUMAR   | MALE   |  
+| VARUN DHAWAN   | MALE   |  
+| VICKEY KAUSHAL | MALE   |  
+| ALIYA BHATT    | FEMALE | 
++----------------+--------+
