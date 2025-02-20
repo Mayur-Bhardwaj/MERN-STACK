aabffe010e97874638d61d99469cebba13620edb
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-02-20 16:26:42 +0530

    update

--- NodeJS/JokeBot/index.js
+++ NodeJS/JokeBot/index.js
@@ -1,1 +1,12 @@
+const TelegramBot = require("node-telegram-bot-api");
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+console.log("Telegram Bot Token:", process.env.TELEGRAM_TOKEN);
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+
+bot.on('message', (option) => {
+    console.log("Message received on the bot", option);
+});
