b8f84621ef88eb69b2de8d89a4cffa96fcaa7e8a
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-05-06 18:33:20 +0530

    Update

--- ReactJs/Projects/HangmanGame/src/App.jsx
+++ ReactJs/Projects/HangmanGame/src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css'
-import Button from './Components/Button/Button';
+// import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Button from './Components/Button';
+import TextInput from './Components/TextInput/TextInput';
+import TextInputForm from './Components/TextInputForm/TextInputForm';
+import TextInputFormContainer from './Components/TextInputForm/TExtInputFormContainer';
+import StartGame from "./Pages/StartGame";
+import PlayGame from "./Pages/PlayGame";
 
-function App() {
+function App(){
 
   return (
-   <div>
-   Hello World
-   <Button />
-   </div>
-  )
+    <>
+      <Routes>
+      <Route path='/start' element={<StartGame />} />
+      <Route path='/play' element={<PlayGame />} />
+      <Route path='/' element={<div>Home</div>} />
+    </Routes>
+    </>
+  );
 }
 
-export default App;
+export default App
