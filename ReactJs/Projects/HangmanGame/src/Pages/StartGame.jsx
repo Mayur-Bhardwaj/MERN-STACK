9f636b79aebb8aaf1f62227cd243fe58ff2dc275
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-05-07 15:19:37 +0530

    Add files via upload

--- ReactJs/Projects/HangmanGame/src/Pages/StartGame.jsx
+++ ReactJs/Projects/HangmanGame/src/Pages/StartGame.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
-
+import TextInputFormContainer from "../Components/TextInputForm/TExtInputFormContainer";
 function StartGame(){
     return(
         <> {/*React Fragment */}
         <h1>Start Game</h1>
+        <TextInputFormContainer />
         <Link to="/play" className="text-blue-400">Play Game Link </Link> {/*Here we also used the anchor tag but the Anchot Tag
             refresh the page but we don't want this so we use Link */}
+        
         </>
         
     );
