e8aca12fe3d4ff8e4d20335961566c0ade1f0961
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-05-07 15:07:33 +0530

    Added the Links

--- ReactJs/Projects/HangmanGame/src/Pages/PlayGame.jsx
+++ ReactJs/Projects/HangmanGame/src/Pages/PlayGame.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 function PlayGame(){
     return(
         <>
             <h1>Play Game</h1>
+            <Link to="/start" className="text-blue-400">Start Game Link</Link>
         </>
     );
 }
--- ReactJs/Projects/HangmanGame/src/Pages/StartGame.jsx
+++ ReactJs/Projects/HangmanGame/src/Pages/StartGame.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 function StartGame(){
     return(
         <> {/*React Fragment */}
         <h1>Start Game</h1>
+        <Link to="/play" className="text-blue-400">Play Game Link </Link> {/*Here we also used the anchor tag but the Anchot Tag
+            refresh the page but we don't want this so we use Link */}
         </>
         
     );
