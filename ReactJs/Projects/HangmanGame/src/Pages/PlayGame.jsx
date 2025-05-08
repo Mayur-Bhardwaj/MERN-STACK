ea3dabbb6ac3b6e0ec71fde78015a50e65f82376
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-05-08 15:28:52 +0530

    Add useLocation() to hide the data inside url

--- ReactJs/Projects/HangmanGame/src/Pages/PlayGame.jsx
+++ ReactJs/Projects/HangmanGame/src/Pages/PlayGame.jsx
@@ -1,9 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useParams, useSearchParams,  } from "react-router-dom";
 
 function PlayGame(){
+
+    // const [searchParams] = useSearchParams();
+    // console.log(searchParams.get("text"));  //This is not uuseful in our game because the text will be shown in the url
+    //The url is http://localhost:5173/play?text=kudf
+
+    //Other Way is Path Params
+//   const { text } =  useParams();
+    //The url is http://localhost:5173/play/Ram
+
+    const { state } = useLocation();
+    console
     return(
         <>
-            <h1>Play Game</h1>
+            {/*// <h1>Play Game {text} </h1> */}
+            <h1> Play Game {state.wordSelected} </h1>
             <Link to="/start" className="text-blue-400">Start Game Link</Link>
         </>
     );
