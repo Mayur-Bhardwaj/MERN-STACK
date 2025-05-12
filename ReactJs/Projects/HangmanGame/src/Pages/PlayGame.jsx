d8f07ed46673c9f20f207c7d0d216e2dc0a90d97
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-05-12 18:01:00 +0530

    update

--- ReactJs/Projects/HangmanGame/src/Pages/PlayGame.jsx
+++ ReactJs/Projects/HangmanGame/src/Pages/PlayGame.jsx
@@ -1,4 +1,5 @@
 import { Link, useLocation, useParams, useSearchParams,  } from "react-router-dom";
+import MaskedText from "../Components/MaskedText/MaskedText";
 
 function PlayGame(){
 
@@ -12,17 +13,19 @@ function PlayGame(){
 
     const { state } = useLocation();
 
-    const arr = ["hello","world"];
+    // const arr = ["hello","world"];
     
     return(
         <>
             {/*// <h1>Play Game {text} </h1> */}
+            <h1> Play Game { state.wordSelected } </h1>
+
             {/*//When we render the list we pass the key */}
-            {arr.map((element, index) => <b key={index}>{element}</b>)}
-            <h1> Play Game {state.wordSelected} </h1>
+
+            // <MaskedText text={state.wordSelected} guessedLetters={['H', 'E']}/>
             <Link to="/start" className="text-blue-400">Start Game Link</Link>
         </>
     );
-}
+}     
 
 export default PlayGame;
